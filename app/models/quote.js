e4919177629786c8ac60178e31be8ecb170b5b14
Kevin Ansfield <kevin@lookingsideways.co.uk>
2014-11-25 21:17:51 +0100

    integrate maths for estimate

--- app/models/quote.js
+++ app/models/quote.js
@@ -6,6 +6,35 @@ export default Ember.Object.extend({
   otherOccupation: null,
   experience: null,
   businessType: null,
-  sumInsured: null
+  publicLiability: null,
+  numberOfEmployees: null,
+  employersLiability: null,
+  professionalIndemnity: null,
+
+
+  publicLiabilityQuote: function () {
+    var publicLiability = parseInt(this.get('publicLiability'));
+    return 1.3 * (publicLiability / 1000);
+  }.property('publicLiability'),
+
+  employersLiabilityQuote: function () {
+    var employersLiability = parseInt(this.get('employersLiability'));
+    var numberOfEmployees = parseInt(this.get('numberOfEmployees'));
+    return 1.0 * (employersLiability / 1000) * numberOfEmployees;
+  }.property('employersLiability', 'numberOfEmployees'),
+
+  professionalIndemnityQuote: function () {
+    var professionalIndemnity = parseInt(this.get('professionalIndemnity'));
+    return 1.8 * (professionalIndemnity / 1000);
+  }.property('professionalIndemnity'),
+
+  quoteTotal: function () {
+    return this.get('publicLiabilityQuote') +
+           this.get('employersLiabilityQuote') +
+           this.get('professionalIndemnityQuote');
+  }.property(
+    'publicLiabilityQuote',
+    'employersLiabilityQuote',
+    'professionalIndemnityQuote')
 
 });
