213233cdda4724803bc99689fcacda73a7a174e9
Kevin Ansfield <kevin@lookingsideways.co.uk>
2014-12-08 16:35:43 +0100

    validations for step-2

--- app/controllers/step-2.js
+++ app/controllers/step-2.js
@@ -1,4 +1,40 @@
 import Ember from 'ember';
 
+var isBlank = Ember.isBlank;
+
 export default Ember.ObjectController.extend({
+
+  isNumberOfEmployeesValid: function () {
+    var number = this.get('numberOfEmployees');
+    return !isBlank(number) && this.isNumber(number);
+  }.property('numberOfEmployees'),
+
+  isSubcontractorsValid: function () {
+    var hasSubcontractors = this.get('hasSubcontractors');
+    var subcontractorsOnSite = this.get('subcontractorsOnSite');
+    if (hasSubcontractors) {
+      return !isBlank(subcontractorsOnSite) &&
+             this.isNumber(subcontractorsOnSite) &&
+             !isBlank(this.get('subcontractorPercentage')) &&
+             this.isNumber(this.get('subcontractorPercentage'));
+    } else {
+      return !isBlank(hasSubcontractors);
+    }
+  }.property(
+    'hasSubcontractors',
+    'subcontractorsOnSite',
+    'subcontractorPercentage'),
+
+  isValid: function () {
+    return this.get('isNumberOfEmployeesValid') &&
+           this.get('isSubcontractorsValid');
+  }.property(
+    'isNumberOfEmployeesValid',
+    'isSubcontractorsValid'),
+
+  isNumber: function (number) {
+    var regexp = /^\d+$/;
+    return isBlank(number) || (''+number).match(regexp);
+  }
+
 });
